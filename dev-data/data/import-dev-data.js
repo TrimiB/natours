3d310aa8da363cd2be59a2749221b19ac051947f
Kushtrim <bujupikushtrim@outlook.com>
2022-08-01 12:00:09 +0200

    added file dev-data/data/import-dev-data.js which is a script to eather import or delete json from or to the database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' }); // configure path for dotenv.
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+/// DB connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection successful'));
+
+///read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+/// imort data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+/// Delete all data from Collection:
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// if 3rd argument of process.argv is ... importData : deleteData
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+/****
+ * To import data from tours-simple.json file to the DB, run:
+ *  node <path of this file> --import
+ * //////////////////////////////////////////////////////////
+ * To delete data from the DB, run:
+ * node <path of this file> --delete
+ */

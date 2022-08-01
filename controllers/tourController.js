66e27bb850e8eb76a34e57f613c2bd2f6f054310
Kushtrim <bujupikushtrim@outlook.com>
2022-08-01 16:23:36 +0200

    Added filtering by incomming URL and added sorting plus default sorting the 'createdAt' property

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,20 +3,33 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     /// Build the query
+    //1A) Filtering
     const queryObj = { ...req.query }; /// create a copy of req.query
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    /// Execute query
+    const tours = await query;
 
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    /// Execute query
-    const tours = await query;
 
     // Send response
     res.status(200).json({

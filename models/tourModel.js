cb587c0b15ece4871af0a909abf6a2cae1427283
Kushtrim <kushtrimbujupi@Kushtrims-MacBook-Pro.fritz.box>
2022-08-01 10:24:04 +0200

    added more properties to the tourModelSchema

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,6 +41,16 @@ const tourSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a Cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);

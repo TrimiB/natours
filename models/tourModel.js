7a8d38e9c3f235f295fca7c4394edc2776e10f7a
Kushtrim <bujupikushtrim@outlook.com>
2022-08-11 16:06:39 +0200

    installed slugify as new dependecy. added new Fileds to the Document schema and new mongoose middleware which hook to the document, query and aggregation functions.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -53,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -60,10 +66,54 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// Virtual Field. Does not get persisted to the DB.
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+////////// MONGOOSE MIDDLEWARE ////////////
+// Document middleware: runs only before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true,
+  });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save Document..');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+///// Query middleware
+tourSchema.pre(/^find/, function (next) {
+  // tourSchema.pre('find', function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`QUERY took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+/// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  console.log(this.pipeline());
+  next();
+});
+
+// model Tour from tourSchema
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;

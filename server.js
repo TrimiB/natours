b0d2c7836d0de846d0205eb2c64aa782e66d79cb
Kushtrim <bujupikushtrim@outlook.com>
2022-08-16 15:17:37 +0200

    Added an Eventlistener on the Nodeprocess to handle node process errors

--- server.js
+++ server.js
@@ -19,4 +19,14 @@ mongoose
   .then(() => console.log('DB Connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App running on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`App running on port ${port}`)
+);
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN ...');
+  server.close(() => {
+    process.exit(1);
+  });
+});

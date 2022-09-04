9aabd30dc4ce1a4b37329a08ff3694036a9d0f54
Kushtrim <bujupikushtrim@outlook.com>
2022-09-04 14:48:43 +0200

    implemented the protect route logic

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,6 +20,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid Token, Please login again,', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,7 +45,7 @@ const sendErrorProd = (err, res) => {
     //Programming or other unknown error: don't leak error detail
   } else {
     // 1) log error
-    console.error('Error !!!');
+    console.error('Error !!!', err);
 
     // 2) send generic message
     res.status(500).json({
@@ -63,6 +69,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
--- models/userModel.js
+++ models/userModel.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
       message: `Wrong password confirmation!`,
     },
   },
+  passwordChangedAt: Date,
 });
 // Mongoose middleware pre 'save' (before presisting data to DB)
 userSchema.pre('save', async function (next) {
@@ -53,6 +54,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimespamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimespamp < changedTimestamp;
+  }
+
+  // False means not changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);

5e61fc066873cbb62d5e3277f5fac630a3417487
Kushtrim <bujupikushtrim@outlook.com>
2022-10-21 13:42:23 +0200

    Added reset password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -153,4 +154,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) get user based on the token.
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) if token has not expired, and there is a user ? set the new password.
+  if (!user) {
+    return next(new AppError(`Token is invalid or has expired`, 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetExpires = undefined;
+  user.passwordResetToken = undefined;
+  await user.save();
+
+  // 3) update changedPasswordat property for the curent user.
+
+  // 4) login the user and send JWT.
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token,
+  });
+});

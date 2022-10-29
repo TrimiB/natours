dcced3b37042ed5e2e41efb1441b97bb749441aa
Kushtrim <bujupikushtrim@outlook.com>
2022-10-29 22:13:17 +0200

    added helmet to protect http

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,12 +11,16 @@ const userRouter = require('./routes/userRouts');
 
 const app = express();
 
-// 1) Global Middlewares
+///////////// 1) Global Middlewares
+// set security HTTP headers
+app.use(helmet());
+
+// development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-/// Request limiter
+/// Request limiter from same API
 const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   windowMs: 60 * 60 * 1000, // 60 Minutes
@@ -23,16 +28,20 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`)); // serving static html from folder
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
   next();
 });
 
-// 2) Routs
+////////////////// 2) Routs
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 

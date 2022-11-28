b00ecc82b2d09262fdaf96fab1ea917567151a2e
Kushtrim <bujupikushtrim@outlook.com>
2022-11-28 15:51:51 +0100

    added the abbility to get the distance from a tour.

--- models/tourModel.js
+++ models/tourModel.js
@@ -182,14 +182,14 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 /// Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({
-    $match: { secretTour: { $ne: true } },
-  });
-  // eslint-disable-next-line no-console
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({
+//     $match: { secretTour: { $ne: true } },
+//   });
+
+//   console.log(this.pipeline());
+//   next();
+// });
 
 // model Tour from tourSchema
 const Tour = mongoose.model('Tour', tourSchema);

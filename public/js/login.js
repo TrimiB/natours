26ac6a62a09a35e601fed21ceba3416f9d7927f1
Kushtrim <bujupikushtrim@outlook.com>
2022-12-21 15:19:35 +0100

    Added ability to change password

--- public/js/login.js
+++ public/js/login.js
@@ -33,7 +33,12 @@ export const logout = async () => {
       url: 'http://127.0.0.1:3000/api/v1/users/logout',
     });
 
-    if (res.data.status === 'success') location.reload(true);
+    if (res.data.status === 'success') {
+      window.setTimeout(() => {
+        location.reload(true);
+      }, 1000);
+      location.assign('/');
+    }
   } catch (error) {
     showAlert('error', 'Error logging out! Try again.');
   }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,21 +2,22 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 /// updating data through our API
-export const updateData = async (name, email) => {
+///// type is eather 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name: name, /// Here we can simply ommit the last name
-        email: email, /// Here we can simply ommit the last email
-      },
+      url: url,
+      data: data,
     });
 
-    // console.log(res);
-
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (error) {
     // console.log(error);

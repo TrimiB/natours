acbd1f7568f2737f37ec66ce93b425f99c395d14
Kushtrim <bujupikushtrim@outlook.com>
2022-12-22 16:13:31 +0100

    Using sharp method to resize incomming images and outputting them to folder with correct naming.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,24 @@
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    // User // ID /// Timestamp
-    // user-9873837494-333333.jpeg
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // User // ID /// Timestamp
+//     // user-9873837494-333333.jpeg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -38,6 +41,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -68,6 +85,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   //2) Filtered our unwanted filed names that are not allowed to be updated.
   const filteredBody = filterObj(req.body, 'name', 'email');
+  // 2:1 If there is a req.file the set req.file.filename to filteredBody.photo
   if (req.file) filteredBody.photo = req.file.filename;
 
   //3) update user document

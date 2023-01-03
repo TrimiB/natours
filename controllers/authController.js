d858d3fd260ca5f69491b5f1cca7ae9109dd7db7
Kushtrim <bujupikushtrim@outlook.com>
2023-01-03 12:23:52 +0100

    added password reset funtionality through email

--- controllers/authController.js
+++ controllers/authController.js
@@ -187,20 +187,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   //3) send it to the user's email.
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new 
-    password and passwordConfirm to ${resetURL}. 
-    \nIf you didn't forget your password, please ignore this.`;
-
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: `Your password reset token (valide for 10min)`,
-    //   message,
-    // });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'Success',

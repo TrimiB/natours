69e590b7f1527d37b87631c06b52bd099e7f87ec
Kushtrim <bujupikushtrim@outlook.com>
2023-01-10 16:54:37 +0100

    deleted cache

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12475,7 +12475,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61351" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62360" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
